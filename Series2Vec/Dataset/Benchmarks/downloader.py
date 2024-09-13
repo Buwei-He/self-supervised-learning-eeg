2c81987572e343dcc7a5ad08092fd314b210ff75
Zheyun <240363246@qq.com>
2024-09-13 21:50:11 +0000

    add missing python files from last version

---
+++ Series2Vec/Dataset/Benchmarks/downloader.py
@@ -0,0 +1,14 @@
+from Opportunity.Opportunity_Loader import Opportunity
+from PAMAP2.PAMAP2_Loader import PAMAP2
+from Skoda.Skoda_Loader import Skoda
+from USC_HAD.USC_HAD_Loader import USC_HAD
+from WISDM.WISDM_Loader import WISDM
+from WISDM2.WISDM2_Loader import WISDM2
+
+if __name__ == '__main__':
+    #Opportunity(window_size=100, step=50)
+    #PAMAP2(window_size=100, step=50)
+    #Skoda(window_size=100, step=50)
+    USC_HAD(window_size=100, step=50)
+    # WISDM(window_size=100, step=50)
+    #WISDM2(window_size=100, step=50)
