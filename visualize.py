f7be6e28d7eb24615552db821a66877295b36312
Buwei He <buwei@kth.se>
2024-11-05 22:17:27 +0000

    Added pairwise 2d analysis and PCA for visualize.py

--- visualize.py
+++ visualize.py
@@ -45,7 +45,7 @@ def visualize(X, y, reducer_type='tsne', problem='Skoda', n_components=2, n_poin
     from umap import UMAP # pip install umap-learn
     from sklearn.manifold import TSNE
 
-    assert reducer_type in ['tsne', 'umap', 'all']
+    assert reducer_type in ['tsne', 'umap']
     assert problem in ['Skoda', 'PAMAP2', 'Oppotunity', 'USC_HAD', 'WISDM', 'WISDM2', 'EEG']
     assert n_components in [2, 3]
     random.seed(random_state)
@@ -54,9 +54,7 @@ def visualize(X, y, reducer_type='tsne', problem='Skoda', n_components=2, n_poin
     y_repr = y.cpu().detach().numpy()
     sample_idx = random.sample(range(len(y_repr)), min(n_points, len(y_repr)))
 
-    if reducer_type == 'umap' or reducer_type == 'all':
-        visualize(X, y, 'tsne', problem, n_components, n_points, random_state)
-        reducer_type = 'umap'
+    if reducer_type == 'umap':
         reducer = UMAP(n_components=n_components) 
     elif reducer_type == 'tsne':
         reducer = TSNE(n_components=n_components)
@@ -77,10 +75,69 @@ def visualize(X, y, reducer_type='tsne', problem='Skoda', n_components=2, n_poin
      
     fig.savefig(os.path.join(root_path, 'visualize', f'{problem}_{reducer_type}_{n_components}d.png'))
 
+def visualize_nd(X, y, reducer_type='tsne', problem='Skoda', n_components=2, n_points=1000, random_state=1234):
+    import os
+    import random
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+    from umap import UMAP
+    from sklearn.manifold import TSNE
+    from sklearn.decomposition import PCA
+
+    assert reducer_type in ['tsne', 'umap', 'pca']
+    assert problem in ['Skoda', 'PAMAP2', 'Oppotunity', 'USC_HAD', 'WISDM', 'WISDM2', 'EEG']
+    assert n_components > 1, "n_components should be at least 2 for pairwise plotting."
+    assert (reducer_type != 'tsne' or n_components < 4), "n_components should be inferior to 4 for tsne."
+    random.seed(random_state)
+
+    X_repr = X.cpu().detach().numpy()
+    y_repr = y.cpu().detach().numpy()
+    sample_idx = random.sample(range(len(y_repr)), min(n_points, len(y_repr)))
+
+    # Initialize the dimensionality reducer
+    if reducer_type == 'umap':
+        reducer = UMAP(n_components=n_components)
+    elif reducer_type == 'tsne':
+        reducer = TSNE(n_components=n_components)
+    elif reducer_type == 'pca':
+        reducer = PCA(n_components=n_components)
+    X_reduced = reducer.fit_transform(X_repr[sample_idx, :])
+
+    # Create a DataFrame for easier plotting
+    columns = [f'dim{i+1}' for i in range(n_components)]
+    df = pd.DataFrame(X_reduced, columns=columns)
+    df['class'] = y_repr[sample_idx]
+
+    # Plot pairwise scatter plots in a matrix format
+    num_plots = (n_components * (n_components - 1)) // 2
+    fig, axes = plt.subplots(n_components - 1, n_components - 1, figsize=(3*n_components, 3*n_components), constrained_layout=True)
+    fig.suptitle(f'{problem} - {reducer_type} - {n_components}D Pairwise Scatter Plots', fontsize=16)
+
+    # Generate pairwise scatter plots
+    for i in range(n_components - 1):
+        for j in range(i + 1, n_components):
+            ax = axes[i, j - 1] if n_components > 2 else axes
+            sns.scatterplot(data=df, x=f'dim{i+1}', y=f'dim{j+1}', hue='class', ax=ax, legend=False)
+            ax.set_title(f'Dim {i+1} vs Dim {j+1}')
+            ax.set_xlabel(f'Dim {i+1}')
+            ax.set_ylabel(f'Dim {j+1}')
+
+    # Hide unused subplots
+    for i in range(n_components - 1):
+        for j in range(i):
+            axes[i, j].axis('off')
+
+    # Save the figure
+    os.makedirs(os.path.join(root_path, 'visualize'), exist_ok=True)
+    fig.savefig(os.path.join(root_path, 'visualize', f'{problem}_{reducer_type}_{n_components}D_pairwise.png'))
+
+
 if __name__ == '__main__':
     problem = "EEG"
-    root_path = "/home/shouzheyun/Series2Vec/"
-    result_path = "Results/Pre_Training/EEG/2024-10-01_22-45"
+    root_path = "/home/buwei/dd2430/self-supervised-learning-eeg"
+    result_path = "Results/Pre_Training/EEG/CF_0.75"
     config = load_config_from_json(root_path, result_path, problem)
     Data = data_loader(config)
     model = Model_factory(config, Data)
@@ -98,10 +155,18 @@ if __name__ == '__main__':
     train_repr, train_labels = S2V_make_representation(SS_Encoder, train_loader)
 
     # ------------------------------- Visualize Test ------------------------------
-    visualize(X=train_repr, 
+    # visualize(X=train_repr, 
+    #         y=train_labels,
+    #         reducer_type='tsne', # 'tsne', 'umap''
+    #         problem=problem, 
+    #         n_components=3, # 2, 3
+    #         n_points=1000
+    #         )
+    
+    visualize_nd(X=train_repr, 
             y=train_labels,
-            reducer_type='tsne', # 'tsne', 'umap''
+            reducer_type='umap', # 'tsne', 'umap', 'pca'
             problem=problem, 
-            n_components=3, # 2, 3
-            n_points=1000
+            n_components=20, # 2, 3
+            n_points=100
             )
